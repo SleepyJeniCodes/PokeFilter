7d12cce115b1d8ea3b11e240ea626a11135c08bf
Jeni <58479587+SleepyJeniCodes@users.noreply.github.com>
2023-08-27 21:35:49 -0400

    Update scripts.js
    script includes Pokemon type images, a card flip that displays the Pokemon's abilities , height, weight, and stats

--- scripts.js
+++ scripts.js
@@ -47,10 +47,6 @@ request.onload = function () {
           const h1 = document.createElement('h1')
           h1.textContent = `${pokemonNum}. ${pokemonDetails.name}`
 
-          // Display Pokemon types in p
-          const typeParagraph = document.createElement('p')
-          typeParagraph.textContent = `Type(s): ${pokemonDetails.types.map(type => type.type.name).join(' | ')}`
-
           // Fetch additional details using the associated link
           fetch(pokemonDetails.species.url)
             .then((response) => response.json())
@@ -59,17 +55,43 @@ request.onload = function () {
               const description = getPokemonDescription(speciesDetails)
               const p = document.createElement('p')
               p.textContent = description
-              
+
               // Create an img element for the Pokemon's picture
               const img = document.createElement('img')
               img.src = pokemonDetails.sprites.front_default
               img.alt = pokemonDetails.name
 
+              // Create array to hold type images
+              const typeImages = pokemonDetails.types.map((type) => {
+                const typeImg = document.createElement('img')
+                typeImg.src = `types/${type.type.name}.png` 
+                typeImg.alt = type.type.name
+                typeImg.classList.add('type-image') // Add class for styling
+                return typeImg
+              })
+
+              // Create a div to hold type images
+              const typeImageContainer = document.createElement ('div')
+              typeImageContainer.classList.add('type-image-container')
+              
+              // Create back content to the card
+              const backContent = createBackContent(pokemonDetails)
+              card.appendChild(backContent)
+
+              // Add click even listener to card to toggle 'flipped' class
+              card.addEventListener('click', () => {
+                card.classList.toggle('flipped')
+              })
+
               // Append the elements to the container element
               container.appendChild(card)
               card.appendChild(h1)
               card.appendChild(img)
-              card.appendChild(typeParagraph)
+
+              typeImages.forEach((typeImg) => {
+                typeImageContainer.appendChild(typeImg)
+              })
+              card.appendChild(typeImageContainer)
               card.appendChild(p)
             })
             .catch((error) => {
@@ -77,7 +99,7 @@ request.onload = function () {
             })
         })
         .catch((error) => {
-          console.error('Error fetching Pokémon details', error);
+          console.error('Error fetching Pokémon details', error)
         })
     })
   } else {
@@ -102,6 +124,72 @@ function getPokemonDescription(pokemonDetails) {
     }
   }
   return "Description not available"
+}
+
+// Define a function to create the back content of the card
+function createBackContent(pokemonDetails) {
+  const backContent = document.createElement('div')
+  backContent.classList.add('back-content')
+
+  const abilities = document.createElement('p')
+  abilities.textContent = `Abilities: ${pokemonDetails.abilities.map(ability => ability.ability.name).join(', ')}`
+  backContent.appendChild(abilities)
+  
+  // Convert height frrom dectograms to feet and inches
+  const heightInDecimeters = pokemonDetails.height
+  const heightInFeet = (heightInDecimeters * 0.328084).toFixed(2)
+  const heightFeet = Math.floor(heightInFeet)
+  const heightInches = Math.round((heightInFeet - heightFeet) * 12)
+
+  // Convert weight from hectograms to pounds
+  const weightInHectograms = pokemonDetails.weight
+  const weightInPounds = (weightInHectograms * 0.22046226218).toFixed(2)
+
+  // Create an element to display height and weight content
+  const heightweightElement = document.createElement('p')
+  heightweightElement.textContent = `Height: ${heightFeet} ft ${heightInches} in || Weight: ${weightInPounds} lbs`
+  backContent.appendChild(heightweightElement)
+
+  // Create a canvas element for the stats chart
+  const statsCanvas = document.createElement('canvas')
+  statsCanvas.id = 'stats-chart'
+  statsCanvas.width = 200
+  statsCanvas.height = 200
+
+  backContent.appendChild(statsCanvas)
+
+  // Create stats chart using Chart.js
+  createStatsChart(statsCanvas, pokemonDetails.stats)
+
+  return backContent
+}
+
+function createStatsChart(canvas, stats) {
+  const labels = stats.map(stat => stat.stat.name)
+  const values = stats.map(stat => stat.base_stat)
+
+  const ctx = canvas.getContext('2d');
+  new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: labels,
+      datasets: [{
+        label: 'Stats',
+        data: values,
+        backgroundColor: 'rgba(75, 192, 192, 0.6)',
+        borderColor: 'rgba(75, 192, 192, 1)',
+        borderWidth: 1
+      }]
+    },
+    options: {
+      responsive: false,
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  })
 }
 
 // Send the request
